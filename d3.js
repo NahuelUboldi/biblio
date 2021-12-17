a1703a327bb245e09d40f13b507d1a5e3342ddd3
NahuelUboldi <nahueluboldi@gmail.com>
2021-12-17 10:05:53 -0300

    d3 updated

--- d3.js
+++ d3.js
@@ -13,3 +13,14 @@ d3.select("body").selectAll("h2")
   .attr("x", (d, i) => {
    return i * 30
 })
+
+//MIN AND MAX FUNCTIONS
+const notNestedData = [4,6,2,3,7,9];
+const outputMin = d3.min(notNestedData) // method to get the min value of an array
+
+const nestedData = [[1, 7, -4],[6, 3, 8],[2, 9, 3]]
+const outputMax = d3.max(nestedData, (d) => d[2]); // cb function to search in nested arrays
+  
+d3.select("body")
+  .append("h2")
+  .text(outputMax)
