821ca03655176b6cf87fca30255917cd8b7265c8
Nahuel Uboldi <nahueluboldi@gmail.com>
2022-05-07 21:18:12 -0300

    more katas

--- test.js
+++ test.js
@@ -1,15 +1,6 @@
-// [9,3,9,3,9,7,9]
-const numbers = {};
-function solution(A) {
-  for (let i = 0; i < A.length; i++) {
-    numbers[A[i]] = (numbers[A[i]] || 0) + 1;
-    if (numbers[A[i]] === 2) {
-      delete numbers[A[i]];
-    }
-  }
-  const res = Object.keys(numbers).find((key) => {
-    return numbers[key] === 1;
-  });
-  return parseInt(res);
-}
-console.log(solution([9, 3, 9, 3, 9, 7, 9]));
+let obj = {};
+console.log(obj.a?.b);
+// console.log(animals.some(key).length === 1);
+// console.log(animals.every(key));
+// console.log(animals.some(key));
+// console.log(animals.filter(key) === true);
