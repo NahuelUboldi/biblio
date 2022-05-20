81d3a8d6474e205b55972545cd3b43b0c497e0e5
NahuelUboldi <nahueluboldi@gmail.com>
2022-05-20 09:15:34 -0300

    some test

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
-let obj = {};
-console.log(obj.a?.b);
-// console.log(animals.some(key).length === 1);
-// console.log(animals.every(key));
-// console.log(animals.some(key));
-// console.log(animals.filter(key) === true);
+const string = "string"
+const number = 5
+const bool = true
+const undefinedVar = undefined
+const symbol = Symbol()
+const bigInt = 7n
+
+const nullVar = null
+
+const object = {}
+const array = []
+
+const func = function() {}
+func.__proto__.__proto__.__proto__
+string.__proto__.__proto__
