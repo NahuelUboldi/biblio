a38dd1c4763ed4f8a8280bd6bdc8b78e1404c53a
NahuelUboldi <nahueluboldi@gmail.com>
2022-06-02 11:01:37 -0300

    tested katas

--- katas-tested/reverseInParentheses.js
+++ katas-tested/reverseInParentheses.js
@@ -2,54 +2,16 @@
 
 // Input strings will always be well-formed with matching ()s.
 
-function reverseString(str) {
-  return str.split('').reverse().join('');
-}
-function flattenParenthesis(string) {
-  if (/\(/.test(string) === false) {
-    return reverseString(string);
-  }
-  return string;
-}
-flattenParenthesis('(hola');
+
 function solution(inputString) {
-  const finalStringArr = [];
-  let counter = 0;
-  let tempS = '';
-  let tempNS = '';
-  for (let i = 0; i < inputString.length; i += 1) {
-    console.log({ i, ltr: inputString[i], counter });
-    // console.log({tempS,tempNS})
-    // console.log("////////////")
-
-    if (inputString[i] === '(') {
-      if (counter === 0) {
-        finalStringArr.push(tempS);
-        tempS = '';
-      }
-      counter += 1;
-    }
-    if (inputString[i] === ')') {
-      if (counter === 1) {
-        // sent to flatten function
-        // return flattened string
-        // push into finalStringArr
-      }
-      counter -= 1;
-    }
-    if (counter === 0 && inputString[i] !== ')') {
-      tempS += inputString[i];
-    }
-    if (counter > 0) {
-      tempNS += inputString[i];
-    }
+  let str = inputString
+  while (/\(/.test(str)) {
+    const closeBracket = str.indexOf(")")
+    const openBracket = str.lastIndexOf("(",closeBracket)
+    const sliced = str.slice(openBracket + 1,closeBracket).split("").reverse().join("")
+    str = str.slice(0,openBracket) + sliced + str.slice(closeBracket + 1)  
   }
-  console.log({ finalStringArr });
-  return inputString;
+  return str
 }
-
-// "a" -> temparr++
-
-solution('foo(bar(baz))blim');
 
 module.exports = solution;
