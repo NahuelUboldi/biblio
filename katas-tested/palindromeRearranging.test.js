13b4f5de5a2ac4df4a1cd5872b968bdfc4180632
NahuelUboldi <nahueluboldi@gmail.com>
2022-06-06 16:30:49 -0300

    solved palindrome rearranging kata with tests: codesignal

---
+++ katas-tested/palindromeRearranging.test.js
@@ -0,0 +1,23 @@
+const solution = require("./palindromeRearranging")
+
+const tests = [
+ ["aabb",true],
+ ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc",false],
+ ["abbcabb",true],
+ ["zyyzzzzz",true],
+ ["z",true],
+ ["zaa",true],
+ ["abca",false],
+ ["abcad",false],
+ ["aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbccccaaaaaaaaaaaaa",false],
+ ["abdhuierf",false]
+]
+
+describe("tests for palindrome rearranging kata", () => {
+ for(const [input,expectedOutput] of tests) {
+  const output = solution(input)
+  it(`should return ${expectedOutput} for ${input} // output: ${output}`, () => {
+   expect(output).toBe(expectedOutput)
+  })
+ }
+})
