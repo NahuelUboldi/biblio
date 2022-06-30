9c9eb01d8de4ebbcb097a8cedd2f97b1dd466c8e
NahuelUboldi <nahueluboldi@gmail.com>
2022-06-30 12:01:48 -0300

    updated bishopAndPawn kata: codesignal

--- katas/bishopAndPawn.js
+++ katas/bishopAndPawn.js
@@ -10,12 +10,9 @@ const validateSquare = (sq) => {
 function solution(bishop, pawn) {
   const cols = 'abcdefgh';
   if (!validateSquare(bishop) || !validateSquare(pawn)) return false;
-  if (bishop[0] === pawn[0] && bishop[1] !== pawn[1]) return false;
-  const bishopCol = cols.indexOf(bishop[0]) + 1;
-  const pawnCol = cols.indexOf(pawn[0]) + 1;
-  const colsDiff = Math.max(bishopCol, pawnCol) - Math.min(bishopCol, pawnCol);
-  const rowsDiff = Math.max(bishop[1], pawn[1]) - Math.min(bishop[1], pawn[1]);
+  const colsDiff = Math.abs(cols.indexOf(bishop[0]), cols.indexOf(pawn[0]));
+  const rowsDiff = Math.abs(bishop[1], pawn[1]);
   return colsDiff === rowsDiff;
 }
 
-console.log({ s: solution('h1', 'g2'), exp: false });
+console.log({ s: solution('h1', 'g2'), exp: true });
